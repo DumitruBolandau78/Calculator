605cb6f4ae3a1b52ab8b124284a30ebf23a3fd9a
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-02-02 18:33:47 +0200

    upload

---
+++ js/index.js
@@ -0,0 +1,45 @@
+const inputShowNewValue = document.querySelector('.answer input'),
+    numbers = document.querySelectorAll('.number'),
+    remove = document.querySelector('.remove'),
+    plus = document.querySelector('.plus'),
+    minus = document.querySelector('.minus'),
+    radical = document.querySelector('.radical'),
+    impartire = document.querySelector('.impartire'),
+    egal = document.querySelector('.egal'),
+    inmultire = document.querySelector('.inmultire');
+
+
+
+inputShowNewValue.value = '';
+
+numbers.forEach(item => {
+    item.addEventListener('click', () => {
+        inputShowNewValue.value += item.textContent;
+    });
+});
+
+remove.addEventListener('click', () => {
+    inputShowNewValue.value = '';
+});
+
+egal.addEventListener('click', () => {
+    if (inputShowNewValue.value == ''){
+        inputShowNewValue.value = '';
+    } else {
+        inputShowNewValue.value = eval(inputShowNewValue.value);
+    }
+});
+
+let arr = [plus, minus, impartire, inmultire, radical];
+
+arr.forEach(item => {
+    item.addEventListener('click', () => {
+        if (inputShowNewValue.value == ''){
+            inputShowNewValue.value = '';
+        } else if (inputShowNewValue.value.slice(-1) == '+' || inputShowNewValue.value.slice(-1) == '-' || inputShowNewValue.value.slice(-1) == '/' || inputShowNewValue.value.slice(-1) == '*' || inputShowNewValue.value.slice(-1) == '%'){
+            inputShowNewValue.value = inputShowNewValue.value.slice(0, -1) + item.textContent;
+        } else {
+            inputShowNewValue.value += item.textContent;
+        }
+    });
+});
